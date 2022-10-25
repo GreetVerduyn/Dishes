71a1441bbffca81fd25e8e450f8dfae85697cf81
Greet Verduyn <greetverduyn@proximus.be>
2022-10-25 16:45:16 +0200

    pass data to next screen with 'navigation'

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -7,7 +7,9 @@ import MealsOverviewScreen from "../data/MealsOverviewScreen";
 function CategoriesScreen({navigation}) {
     function renderCategoryItem(itemData) {
         function pressHandler() {
-            navigation.navigate('MealsOverview');
+            navigation.navigate('MealsOverview',{
+                categoryId: itemData.item.id,
+            });
         }
 
         return (
